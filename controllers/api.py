1ef8db6a09f3ab4b3bfa9637b92da7ba172e8b87
Marko Jerkovic <markojerkovic@eduroam-169-233-207-202.ucsc.edu>
2016-10-28 13:07:07 -0700

    First commit, a lot of the functionality working

--- controllers/api.py
+++ controllers/api.py
@@ -1,11 +1,36 @@
 # These are the controllers for your ajax api.
+from aetypes import end
+
 
 def get_posts():
     """This controller is used to get the posts.  Follow what we did in lecture 10, to ensure
     that the first time, we get 4 posts max, and each time the "load more" button is pressed,
     we load at most 4 more posts."""
     # Implement me!
-    return response.json(dict())
+    start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
+    end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
+    posts = []
+    has_more = False
+    rows = db().select(db.post.ALL, limitby=(start_idx, end_idx + 1))
+    for i, r in enumerate(rows):
+        if i < end_idx - start_idx:
+            t = dict(
+                id = r.id,
+                user_email = r.user_email,
+                post_content = r.post_content,
+                created_on = r.created_on,
+                updated_on = r.updated_on,
+            )
+            posts.append(t)
+        else:
+            has_more = True
+        logged_in = auth.user_id is not None
+    return response.json(dict(
+        posts = posts,
+        logged_in = logged_in,
+        has_more = has_more,
+    ))
+
 
 
 # Note that we need the URL to be signed, as this changes the db.
@@ -13,12 +38,16 @@ def get_posts():
 def add_post():
     """Here you get a new post and add it.  Return what you want."""
     # Implement me!
-    return response.json(dict())
+    p_id = db.post.insert(
+        post_content = request.vars.post_content
+    )
+    p = db.post(p_id)
+    return response.json(dict(post=p))
 
 
 @auth.requires_signature()
 def del_post():
     """Used to delete a post."""
-    # Implement me!
-    return response.json(dict())
+    db(db.post.id == request.vars.post_id).delete()
+    return "ok"
 
--- static/js/default_index.js
+++ static/js/default_index.js
@@ -13,6 +13,72 @@ var app = function() {
         }
     };
 
+    function get_post_url(start_idx, end_idx) {
+        var pp = {
+            start_idx: start_idx,
+            end_idx: end_idx
+        }
+        console.log(get_posts_url + "?" + $.param(pp))
+        return get_posts_url + "?" + $.param(pp)
+    }
+
+    //only called initially
+    self.get_posts = function () {
+       $.getJSON(get_post_url(0, 4), function(data) {
+            self.vue.posts = data.posts;
+            self.vue.has_more = data.has_more;
+            self.vue.logged_in = data.logged_in;
+        })
+
+    };
+
+    //loads four more posts
+    self.get_more = function () {
+        var num_posts = self.vue.posts.length
+        $.getJSON(get_post_url(num_posts, num_posts + 4), function(data){
+            self.vue.has_more = data.has_more;
+            self.extend(self.vue.posts, data.posts);
+        })
+    };
+
+    self.add_post_button = function() {
+        self.vue.is_adding_post = !self.vue.is_adding_post;
+    }
+
+    //uses ajax to add a post to the post database, then the post is returned from the
+    //server once added to the database. it is then added to the front of the post list
+    self.add_post = function () {
+        $.post(add_post_url,
+            {
+                post_content: self.vue.form_content
+            },
+            function (data) {
+                $.web2py.enableElement($("#add_post_submit"));
+                self.vue.posts.unshift(data.post);
+            }
+        )
+    }
+
+    self.delete_post = function(post_id) {
+
+        $.post(del_post_url,
+            {
+                post_id: post_id
+            },
+            function () {
+                var idx = null;
+                for(var i = 0; i < self.vue.posts.length; i++) {
+                    if (post_id === self.vue.posts[i].id){
+                        idx = i + 1;
+                        break;
+                    }
+                }
+                if (idx) {
+                        self.vue.posts.splice(idx - 1, 1);
+                }
+            }
+        )
+    }
 
     // Complete as needed.
     self.vue = new Vue({
@@ -20,14 +86,23 @@ var app = function() {
         delimiters: ['${', '}'],
         unsafeDelimiters: ['!{', '}'],
         data: {
-            has_more: false
+            posts: [],
+            has_more: false,
+            is_adding_post: false,
+            form_content: null
         },
         methods: {
-            get_more: self.get_more
+            get_more: self.get_more,
+            add_post_button: self.add_post_button,
+            add_post: self.add_post,
+            delete_post: self.delete_post
         }
 
     });
 
+    //uncomment when all this shit works
+    self.get_posts();
+    $("#vue-div").show();
 
     return self;
 };
